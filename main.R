683062f449d4af2763d431dfb4a9ca64291a8e16
antagomir <leo.lahti@iki.fi>
2021-11-20 14:39:38 +0200

    now benchmarking report is independent of the test

--- main.R
+++ main.R
@@ -6,23 +6,25 @@ source("experiments/funcs.R")
 source("experiments/data.R")
 
 # Define all tests
+tests <- list()
 tests[["melt"]] <- c(tse = melt_tse_exec_time, pseq = melt_pseq_exec_time)
 # Add these one by one making sure that they work
 # tests[["transform"]] <- c(tse = transform_tse_exec_time, pseq = transform_pseq_exec_time)
 # tests[["agglomerate"]] <- c(tse = agglomerate_tse_exec_time, pseq = agglomerate_pseq_exec_time)
 # tests[["alpha"]] <- c(tse = alpha_tse_exec_time, pseq = alpha_pseq_exec_time)
 # tests[["beta"]] <- c(tse = beta_tse_exec_time, pseq = beta_pseq_exec_time)
+# Other functionality to test..? Tree-based functions? It would then be necessary to ensure that all example data sets have tree info.
 
 # render output of "melt_benchmark.Rmd" as a md document
 for (testmethod in names(tests)) {
+  print(testmethod)
   # Define test file names
   testfile <- paste0(testmethod, "_benchmark_run.R")
-  testrmd <- paste0("experiments/", testmethod, "_benchmark.Rmd")
   # Run benchmarking tests
   source(testfile) 
   # Report benchmarking tests
-  rmarkdown::render(testrmd, output_format = "md_document", output_dir = "reports")
-  rmarkdown::render(testrmd, output_format = "pdf_document", output_dir = "reports")
+  rmarkdown::render("experiments/benchmark.Rmd", output_format = "md_document",  output_file = paste0("../reports/", testmethod, ".md"))
+  rmarkdown::render("experiments/benchmark.Rmd", output_format = "pdf_document", output_file = paste0("../reports/", testmethod, ".pdf"))
 }
 
 
