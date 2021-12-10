5f05aa8a8f51ca7f0d2edc20aa11341c77f30ff0
Giulio Benedetti <giuliobene2000@gmail.com>
2021-12-10 17:33:50 +0200

    Expand experiment to more sample sizes and data sets

--- experiments/bigdata.R
+++ experiments/bigdata.R
@@ -6,7 +6,7 @@ bigdata <- containers[ind]
 
 # define sample sizes
 max_sample_size <- ncol(bigdata)
-big_sample_sizes <- c(10, 100, 1000, max_sample_size)
+big_sample_sizes <- c(10, 100, 1000, 10000, max_sample_size)
 
 # run analysis
 big_df <- experiment_benchmark(bigdata, tests[[testmethod]], big_sample_sizes)
