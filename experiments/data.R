0e420cd7dad37869688b11e0d2fef47f1a72819c
Giulio Benedetti <giuliobene2000@gmail.com>
2021-12-10 21:25:00 +0200

    Reduce computational load by decreasing number of data sets

--- experiments/data.R
+++ experiments/data.R
@@ -23,8 +23,8 @@ library(microbiome)             # transform functions
 # list data sets to run benchmark on
 # data_sets <- c("AsnicarF_2017", "GlobalPatterns", "AsnicarF_2021")
 # data_sets <- "SongQAData" # Just pick a single data set to keep things simple. Must have N>1000 samples.
-# data_sets <- c("AsnicarF_2021", "SongQAData", "GrieneisenTSData") # All data sets must have N>1000 samples.
-data_sets <- c("AsnicarF_2021", "SongQAData", "GrieneisenTSData", "HMP_2019_ibdmdb", "LifeLinesDeep_2016", "ShaoY_2019") 
+data_sets <- c("AsnicarF_2021", "SongQAData", "GrieneisenTSData") # All data sets must have N>1000 samples.
+# data_sets <- c("AsnicarF_2021", "SongQAData", "GrieneisenTSData", "HMP_2019_ibdmdb", "LifeLinesDeep_2016", "ShaoY_2019") 
 
 # define experimental setup
 set.seed(3)
