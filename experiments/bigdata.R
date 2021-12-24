239d784bc221adffdbd4f13d8d5e714c45596505
antagomir <leo.lahti@iki.fi>
2021-12-24 12:39:36 +0200

    update experiment

--- experiments/bigdata.R
+++ experiments/bigdata.R
@@ -1,8 +1,3 @@
 # Separate analysis for the largest data set
-
-# define sample sizes
-big_sample_sizes <- c(100, 1000, 10000, ncol(bigdata))
-#big_sample_sizes <- c(10,20,30)
-
 # run analysis
 big_df <- experiment_benchmark(list(bigdata), tests[[testmethod]], big_sample_sizes)
