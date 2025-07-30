e1127d0896457d15b4579118caa646c9012afb5d
Giulio Benedetti <giuliobene2000@gmail.com>
2025-07-30 20:54:20 +0300

    Set seed and extend tse beta benchmark for article

--- article/OMA_figure.R
+++ article/OMA_figure.R
@@ -9,13 +9,13 @@ library(phyloseq)
 library(picante)
 library(tidyr)
 
+# Set seed for reproducibility
+set.seed(123)
+
 # Set benchmarking hyperparameters
 n_iter <- 10
-beta_threshold <- 1000
+beta_threshold <- c(1000, 5000)
 N <- c(100, 225, 500, 1000, 2250, 5000, 10000)
-# N <- c(100, 200, 300)
-# N <- c(100, 500, 1000)
-# N <- c(100, 500, 1000, 5000, 10000)
 
 # Define method names
 methods <- c(alpha = "Faith diversity",
@@ -58,18 +58,23 @@ benchmark_out <- bench::press(
       agglomerate_tse = quote(mia::agglomerateByRank(tse, rank = "Phylum"))
     )
     
-    if( N <= beta_threshold ){
+    if( N <= beta_threshold[[1]] ){
       # Estimate unifrac from phyloseq
       expressions[["beta_pseq"]] = quote(
         phyloseq::UniFrac(pseq)
       )
+    }else{
+      # Use dummy expression because all results must have the same length
+      expressions[["beta_pseq"]] = quote(1 + 1)
+    }
+    
+    if( N <= beta_threshold[[2]] ){
       # Estimate unifrac from TreeSE
       expressions[["beta_tse"]] <- quote(
         mia::getDissimilarity(tse, method = "unifrac")
       )
-    } else {
-      # Use dummy expressions because all results must have the same length
-      expressions[["beta_pseq"]] = quote(1 + 1)
+    }else{
+      # Use dummy expression because all results must have the same length
       expressions[["beta_tse"]] <- quote(1 + 1)
     }
     
@@ -89,12 +94,14 @@ benchmark_df <- benchmark_out %>%
   dplyr::select(expression, N, time) %>%
   separate_wider_delim(cols = expression, delim = "_",
                        names = c("method", "object")) %>%
-  filter(method != "beta" | N <= beta_threshold)
+  filter(method != "beta" |
+           (object == "pseq" & N <= beta_threshold[[1]]) |
+           (object == "tse" & N <= beta_threshold[[2]]))
 
 # Write to file
-write.csv(as.data.frame(benchmark_df),
-          file = "article/benchmark_rawdata.csv",
-          row.names = FALSE)
+benchmark_df %>%
+  mutate(time = as.numeric(time)) %>%
+  write.csv(file = "article/benchmark_rawdata.csv", row.names = FALSE)
 
 # Summarise benchmarking results with mean time and standard deviation
 benchmark_df <- benchmark_df %>%
@@ -105,9 +112,9 @@ benchmark_df <- benchmark_df %>%
   mutate(method = factor(method, levels = names(methods)))
 
 # Write to file
-write.csv(benchmark_df,
-          file = "article/benchmark_results.csv",
-          row.names = FALSE)
+benchmark_df %>%
+  mutate(Time = as.numeric(Time)) %>%
+  write.csv(file = "article/benchmark_results.csv", row.names = FALSE)
 
 # Visualise benchmarking results
 ggplot(benchmark_df, aes(x = N, y = Time, colour = object)) +
@@ -123,4 +130,5 @@ ggplot(benchmark_df, aes(x = N, y = Time, colour = object)) +
 
 # Save plot to file
 ggsave("article/OMA_figure.png",
-       width = 6, height = 10)
+       width = 6,
+       height = 10)
