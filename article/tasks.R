2a78101e97c5478d4b0836ed1cc301f827845b42
Giulio <giuliobene2000@gmail.com>
2026-04-17 19:07:06 +0300

    Order tasks

--- article/tasks.R
+++ article/tasks.R
@@ -1,8 +1,8 @@
 
-obj.types <- c("tse", "pseq", "spseq", "qiime", "mothur")
+obj.types <- c("tse", "pseq", "spseq")#, "qiime", "mothur")
 obj.funs <- c("alpha", "beta", "trans", "agg", "melt")
-row.sizes <- format(10^(1:4), scientific = FALSE, trim = TRUE)
-col.sizes <- format(10^(1:5), scientific = FALSE, trim = TRUE)
+row.sizes <- 10^(1:4)
+col.sizes <- 10^(1:5)
 rand.states <- 0
 
 df <- expand.grid(
@@ -18,6 +18,11 @@ to_remove <- (df$object == "spseq" & df$method %in% c("alpha", "beta", "trans"))
     (df$object %in% c("qiime", "mothur") & df$method %in% c("trans", "melt"))
 
 df <- df[!to_remove, ]
+
+df <- df[order(df$cols * df$rows, df$method, df$object), ]
+
+df$rows <- format(df$rows, scientific = FALSE, trim = TRUE)
+df$cols <- format(df$cols, scientific = FALSE, trim = TRUE)
 
 lines <- apply(df, 1L, paste, collapse = " ")
 writeLines(lines, "tasks.txt")
