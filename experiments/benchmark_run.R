728b4607e4ddf7085deeb976560ef6e83ae8e789
Giulio Benedetti <giuliobene2000@gmail.com>
2026-05-07 21:11:59 +0300

    Move to benchmark paper version

--- experiments/benchmark_run.R
+++
@@ -1,4 +0,0 @@
-# run benchmark of tse and pseq with custom sample sizes
-df_benchmark <- experiment_benchmark(containers,
-                                     tests[[testmethod]], 
-                                     sample_sizes)
--- experiments/bigdata.R
+++
@@ -1,3 +0,0 @@
-# Separate analysis for the largest data set
-# run analysis
-big_df <- experiment_benchmark(list(bigdata), tests[[testmethod]], big_sample_sizes)
